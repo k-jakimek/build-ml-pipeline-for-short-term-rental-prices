4e7df2d1955e2f7cd0a833c43937fb489c83774b
KJakimek <k.jakimek@gmail.com>
2026-09-21 11:40:08 +0200

    fixed issue in run

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -20,13 +20,17 @@ def go(args):
     # particular version of the artifact
     artifact_local_path = run.use_artifact(args.input_artifact).file()
     df = pd.read_csv(artifact_local_path)
-    idx = df['price'].between(args.min_price, args.max_price)
+    # Basic cleaning
+    df = df.drop_duplicates()
+    df = df.dropna(subset=["price"])
+    df = df[df["price"].between(args.min_price, args.max_price)]
 
     # Remove data points outside the expected NYC boundaries
     idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].between(40.5, 41.2)
     df = df[idx].copy()
+
     logger.info("Cleaned data has %s rows and %s columns", *df.shape)
-    
+
     df.to_csv("clean_sample.csv", index=False)
     artifact = wandb.Artifact(args.output_artifact,
                 type=args.output_type, description=args.output_description)
